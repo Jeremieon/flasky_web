6cfca19bd22ddea1bc25cd3852a9b940f575968b
Jeremieon <jerry_slade@outlook.com>
2023-06-13 13:40:55 +0200

    added database and security

--- app.py
+++ app.py
@@ -3,13 +3,33 @@ import os
 import psycopg2
 from flask import Flask,render_template,request,url_for,flash, session
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 # Load environmental variables from .env file
 load_dotenv()
-#intialized the app name
-app = Flask(__name__)
 
 # Access your variables
 secret_key = os.environ.get('SECRET_KEY')
 database_url = os.environ.get('DATABASE_URL')
+
+#intialized the app name
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
+db = SQLAlchemy(app)
+
+#models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+# Create Database Tables
+db.create_all()
+
+@app.route('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
