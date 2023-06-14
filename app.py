bc1adc60adf9ce11c9fe4eec07b31896f0c8f921
Jeremieon <jerry_slade@outlook.com>
2023-06-15 01:06:38 +0200

    i always forget to commit at every stages danm this alot i have done here API,Auth,Front end etc

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #import required modules
 import os
 #import psycopg2
-from flask import Flask,render_template,request,url_for,flash, session,redirect
+from flask import Flask,render_template,request,url_for,flash, session,redirect,jsonify
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash,check_password_hash
 
 
@@ -28,16 +29,91 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True,nullable=False)
     email = db.Column(db.String(120), unique=True,nullable=False)
     password = db.Column(db.String(300), nullable=False)
+    posts = db.relationship('Post', backref='user',lazy=True)
+    profile = db.relationship('Profile', backref='user')
 
     def __init__(self, username, password,email):
         self.username = username
         self.email = email
         self.password = password
-        
+
+#Profile
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    profession = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
+    user_no = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, firstname, lastname,email,age,profession,country,bio):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.age = age
+        self.profession = profession
+        self.country = country
+        self.bio = bio
+
+
+#library
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(50))
+    author =  db.Column(db.String(100))
+    title = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+    added_date = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    def __init__(self, category, author,title,year):
+        self.id = id
+        self.category = category
+        self.author = author
+        self.title = title
+        self.year = year
+
+    def serialize(self):
+        return {
+            'id':self.id,
+            'category': self.id,
+            'author': self.author,
+            'title': self.title
+        }
+# Posts
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comments = db.relationship('Comment', backref='post')
+
+    def __init__(self,title,content):
+        self.content = content
+        self.title = title
+#comments
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    commented_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+    def __init__(self,content):
+        self.content = content
 
 # Create Database Tables
-db.create_all()
 
+db.create_all()
+#======================================Methods starts here====================================#
+
+#===============User Auth===================#
 # Route to create a new post for a user
 @app.route('/register', methods=['GET', 'POST'])
 def create_user():
@@ -85,3 +161,75 @@ def logout():
     session.pop('username', None)
     flash('Logged out successfully.')
     return redirect(url_for('login'))
+#===============End of User Auth===================#
+
+#===============Library===================#
+#Get all books
+@app.route('/library')
+def get_books():
+    books = Books.query.all()
+    return render_template('library.html',books=books)
+
+#Get Api books
+@app.route('/api/books', methods=['GET'])
+def api_get_books():
+    books = Books.query.all()
+    serialized_books = [book.serialize() for book in books]
+    return jsonify(serialized_books),200
+
+#get one particular book
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    books = Books.query.filter_by(id=book_id).first()
+    if books:
+        serialized_books = books.serialize()
+        return jsonify(serialized_books),200    
+    return jsonify({"message": "Book not found"}), 404
+
+
+# create a new Book
+@app.route('/books/', methods=['POST'])
+def create_book():
+    data = request.get.json()
+    category = data.get('category')
+    author = data.get('author')
+    title = data.get('title')
+    year = data.get('year')
+    new_book = Books(category=category,author=author,title=title,year=year)
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return jsonify(new_book.serialize()),201
+
+#update book
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_user(book_id):
+    book = Books.query.get(id=book_id)
+    if book:
+        data = request.get.json()
+        category = data.get('category')
+        author = data.get('author')
+        title = data.get('title')
+        year = data.get('year')
+
+        book.category = category
+        book.author = author
+        book.title = title
+        book.year = year
+
+        db.session.commit()
+
+        return jsonify(user.serialize())
+    return jsonify({'message': 'User not found'}), 404
+
+#delete book
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_books(book_id):
+    book = Books.query.get(id=book_id)
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return jsonify({'message': 'Book deleted'})
+    return jsonify({'message': 'Book not found'}), 404
+#===============End of Library===================#
